7ca81c574a7287ad471e3bf862d93882ba5b3828
bstascavage <brian@stascavage.com>
2024-04-16 15:37:40 -0400

    Fixing table styling for player list

--- src/Banner.js
+++ src/Banner.js
@@ -18,12 +18,24 @@ function Banner(props) {
     },
   });
 
+  // For some reason I could only get the MUI style working by setting the style directly.
+  // I would like it to be a proper CSS class but either you can't or I'm dumb
+  const iconStyle = {
+    maxHeight: "50px",
+    height: "auto",
+    width: "auto",
+  };
+
   let banner = (
     <Box sx={{ flexGrow: 1 }}>
       <ThemeProvider theme={theme}>
         <AppBar position="static" color="banner">
           <Toolbar className="mona-lisa-banner">
-            <SvgIcon component={props.logo} inheritViewBox></SvgIcon>
+            <SvgIcon
+              style={iconStyle}
+              component={props.logo}
+              inheritViewBox
+            ></SvgIcon>
           </Toolbar>
         </AppBar>
       </ThemeProvider>
